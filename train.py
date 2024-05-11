f0cbfa41f0aed9cbb10da968d70976203bef139e
태현 김 <th.kim@hyperinfo.co.kr>
2024-05-12 07:05:12 +0900

    model save both type

--- train.py
+++ train.py
@@ -1,14 +1,8 @@
-import os
 from PIL import Image
 import core as cc
-import tensorflow as tf
-# tf.get_logger().setLevel('ERROR')
-# tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
-from tensorflow import keras
 
 PATIENCE = 7
 CAPTCHA_TYPE = cc.CaptchaType.NH_WEB_MAIL
-WEIGHT_ONLY = False
 
 train_img_path_list = cc.get_image_files(CAPTCHA_TYPE, train=True)
 img = Image.open(train_img_path_list[0])
@@ -17,5 +11,7 @@ img_height = img.height
 
 CM = cc.CreateModel(train_img_path_list, img_width, img_height)
 model = CM.train_model(epochs=100, earlystopping=True, early_stopping_patience=PATIENCE)
-weights_path = cc.get_weights_path(CAPTCHA_TYPE, WEIGHT_ONLY)
+weights_path = cc.get_weights_path(CAPTCHA_TYPE, True)
 model.save_weights(weights_path)
+weights_path = cc.get_weights_path(CAPTCHA_TYPE, False)
+model.save(weights_path)
