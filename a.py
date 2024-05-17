e39a79e4ed65bfe6229d146e74cda9c4dc0732b6
th.kim <th.kim@hyperinfo.co.kr>
2024-05-17 13:40:16 +0900

    변수 정리

--- a.py
+++
@@ -1,1 +0,0 @@
-print("한글도 되나요? Hello from a.py")
--- captchaCracker.py
+++ captchaCracker.py
@@ -5,25 +5,27 @@ import sys
 import time
 from hyper import CaptchaType, Hyper, ApplyModel
 
-start = time.time()
+START_TIME = time.time()
 ARGV = sys.argv
 CAPTCHA_TYPE = CaptchaType.SUPREME_COURT
 WEIGHT_ONLY = True
-hyper = Hyper()
-pred = ""
+HYPER = Hyper()
+PRED = ""
+END_TIME = None
 
 def main(captchaType:CaptchaType, weight_only, imagePath:str):
 
+    pred = ""
 
     try:
-        train_img_path_list = hyper.get_image_files(captchaType, train=True)
-        img_width, img_height = hyper.get_image_info(train_img_path_list)
-        max_length, characters = hyper.get_train_info(train_img_path_list)
-        weights_path = hyper.get_weights_path(captchaType, weight_only)
+        train_img_path_list = HYPER.get_image_files(captchaType, train=True)
+        img_width, img_height = HYPER.get_image_info(train_img_path_list)
+        max_length, characters = HYPER.get_train_info(train_img_path_list)
+        weights_path = HYPER.get_weights_path(captchaType, weight_only)
         model = ApplyModel(weights_path, img_width, img_height, max_length, characters)
         pred = model.predict(imagePath)
     except Exception as e:
-        hyper.quiet(False)
+        HYPER.quiet(False)
         print("Error:", e)
 
     return pred
@@ -33,15 +35,15 @@ if len(ARGV) < 3:
     sys.exit(-1)
 
 if("__main__" == __name__):
-    hyper.quiet(True)
+    HYPER.quiet(True)
     CAPTCHA_TYPE = CaptchaType(ARGV[1])
     imagePath = ARGV[2]
-    pred = main(CAPTCHA_TYPE, WEIGHT_ONLY, imagePath)
-    hyper.quiet(False)
-    print(pred)
-    # end = time.time()
-    # print("time : ", end - start, "sec")
-    # print(pred, CAPTCHA_TYPE, WEIGHT_ONLY, imagePath)
+    PRED = main(CAPTCHA_TYPE, WEIGHT_ONLY, imagePath)
+    HYPER.quiet(False)
+    print(PRED)
+    END_TIME = time.time()
+    print("time : ", END_TIME - START_TIME, "sec")
+    print(PRED, CAPTCHA_TYPE, WEIGHT_ONLY, imagePath)
     sys.exit(0)
 
 else:
