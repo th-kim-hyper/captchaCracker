ed5246704f184605d9d493e4c784b8ff7ebfd5e7
th.kim <th.kim@hyperinfo.co.kr>
2024-08-29 14:24:33 +0900

    default add

--- pred.py
+++ pred.py
@@ -2,7 +2,7 @@ from util import get_train_data_list
 from core import Model
 
 train_data_list = get_train_data_list()
-train_data = train_data_list['wetax']
+train_data = train_data_list['default']
 
 model = Model(train_data=train_data, quiet_out=False)
 model.validate_model()
