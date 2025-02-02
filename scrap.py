cd0d928d03b7d839c42e8ffa279c15ae350f5ae6
태현 김 <th.kim@hyperinfo.co.kr>
2025-02-02 18:40:23 +0900

    scrap 추가

--- scrap.py
+++ scrap.py
@@ -3,48 +3,30 @@ from datetime import datetime
 from time import sleep
 from playwright.sync_api import sync_playwright
 
-def image_save():
-    btn = iframe_element.wait_for_selector('input.w2trigger.btn_fcm.ty1')
-    btn.click()
-    sleep(1)
+# def image_save():
+#     btn = iframe_element.wait_for_selector('input.w2trigger.btn_fcm.ty1')
+#     btn.click()
+#     sleep(1)
 
-    img = iframe_element.wait_for_selector('img.w2image.mr10.pb5')
-    timestamp = datetime.now().strftime('%Y%m%d_%H%M%S_%f')[:-3]
-    scrap_path = r'C:\python\captchaCracker\images\supreme_court\scrap'
-    image_path = os.path.join(scrap_path, f'{timestamp}.png')
-    img.screenshot(path=image_path)
-    # data_url = img2DataUrl(img)
+#     img = iframe_element.wait_for_selector('img.w2image.mr10.pb5')
+#     timestamp = datetime.now().strftime('%Y%m%d_%H%M%S_%f')[:-3]
+#     scrap_path = r'C:\python\captchaCracker\images\supreme_court\scrap'
+#     image_path = os.path.join(scrap_path, f'{timestamp}.png')
+#     img.screenshot(path=image_path)
+#     # data_url = img2DataUrl(img)
     
-    # save_base64_to_png(data_url.replace('data:image/png;base64,', ''), image_path)
-    return image_path
+#     # save_base64_to_png(data_url.replace('data:image/png;base64,', ''), image_path)
+#     return image_path
 
-def save_base64_to_png(base64_str, file_path):
-    img_data = base64.b64decode(base64_str)
-    with open(file_path, 'wb') as f:
-        f.write(img_data)
+# def save_base64_to_png(base64_str, file_path):
+#     img_data = base64.b64decode(base64_str)
+#     with open(file_path, 'wb') as f:
+#         f.write(img_data)
 
-# def img2DataUrl(img):
-#     script = '''
-#     img => {
-#         const canvas = document.createElement('canvas');
-#         const ctx = canvas.getContext('2d');
-#         canvas.width = img.width;
-#         canvas.height = img.height;
-#         // 흰색 배경 그리기
-#         ctx.fillStyle = '#FFFFFF';
-#         ctx.fillRect(0, 0, canvas.width, canvas.height);
-#         // 이미지 그리기
-#         ctx.drawImage(img, 0, 0);
-#         return canvas.toDataURL('image/png');
-#     }
-#     '''
-#     return img.evaluate(script)
-
-def img2DataUrl(image_path):
-    with open(image_path, 'rb') as img_file:
-        img_data = img_file.read()
-    return 'data:image/png;base64,' + base64.b64encode(img_data).decode('utf-8')
-
+# def img2DataUrl(image_path):
+#     with open(image_path, 'rb') as img_file:
+#         img_data = img_file.read()
+#     return 'data:image/png;base64,' + base64.b64encode(img_data).decode('utf-8')
 
 def predictCaptcha(captcha_id, captcha_image_path):
     url = 'https://dev.hyperinfo.co.kr/captcha/api/predict' if 'https' in captcha_image_path else 'http://dev.hyperinfo.co.kr:12004/api/predict'
@@ -67,30 +49,33 @@ def predictCaptcha(captcha_id, captcha_image_path):
         print('Error:', response.status_code, response.text)
         return None
 
-# file_list = glob.glob('images/*.png')
-# for file in file_list:
-#     if '[UNK]' not in file:
-#         shutil.move(file, os.path.join("images", 'train', os.path.basename(file)))
-    # else:
-    #     shutil.move(file, os.path.join("images", 'train', f'{pred}.png'))
+def scrap_supreme_court(count):
 
+    url = "https://www.scourt.go.kr/portal/information/events/search/search.jsp"
 
-# for file in file_list:
-#     pred = predictCaptcha('supreme_court', file)
-#     print(f'origin = {file},    pred = {pred}')
-#     shutil.move(file, os.path.join('images', f'{pred}.png'))
+    with sync_playwright() as p:
+        browser = p.chromium.launch()
+        page = browser.new_page()
+        page.goto(url)
+        iframe = page.frames[1]
+        result = []
 
-url = "https://www.scourt.go.kr/portal/information/events/search/search.jsp"
+        for i in range(count):
+            btn = iframe.wait_for_selector('input.w2trigger.btn_fcm.ty1')
+            btn.click()
+            sleep(1)
+            img = iframe.wait_for_selector('img.w2image.mr10.pb5')
+            timestamp = datetime.now().strftime(r'%Y%m%d_%H%M%S_%f')[:-3]
+            scrap_path = os.path.join('images', 'supreme_court', 'scrap')
+            image_path = os.path.join(scrap_path, f'{timestamp}.png')
+            img.screenshot(path=image_path)
+            result.append(image_path)
+            print(f'{i+1}/{count} Scraped: {image_path}')
+    return result
 
-with sync_playwright() as p:
-    browser = p.chromium.launch()
-    page = browser.new_page()
-    page.goto(url)
+def scrap(captcha_id, count):
 
-    iframe_element = page.frames[1]
+    if captcha_id == 'supreme_court':
+        scrap_supreme_court(count)
 
-    for i in range(2):
-        image_save()
-        print(f'{i+1}번째 이미지 저장 완료')  
-
-    browser.close()
+scrap('supreme_court', 10)
