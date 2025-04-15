bc06b7e3ee56d77cec8e1c1cf902a1392dffa205
태현 김 <th.kim@hyperinfo.co.kr>
2025-04-15 19:31:43 +0900

    kshop model 추가가

--- pred.py
+++ pred.py
@@ -3,7 +3,7 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 from cc.Core import Model, get_captcha_type_list
 
-captcha_id = 'gov24'
+captcha_id = 'kshop'
 captcha_type_list = get_captcha_type_list()
 train_data = captcha_type_list[captcha_id].train_data
 
